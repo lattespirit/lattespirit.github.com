78a291d15185c2d08340afc2ad8aee81f6be47c2
Jeffrey Yeung <lattespirit@gmail.com>
2025-04-20 19:14:54 +0800

    feat: add page transition animation

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,4 +1,3 @@
-/* eslint-disable react/prop-types */
 import React from "react";
 import { graphql, useStaticQuery } from "gatsby";
 import { Helmet } from "react-helmet";
@@ -7,8 +6,40 @@ import Navbar from "./Navbar";
 import Footer from "./Footer";
 import Search from "./Search";
 import Toast from "./Toast";
+import { AnimatePresence, motion } from "motion/react";
 
-export default ({ children }) => {
+const duration = 0.3;
+const ease = [0.76, 0, 0.24, 1];
+
+const variants = {
+  initial: {
+    opacity: 0,
+    scale: 1.01,
+    transition: {
+      ease,
+      duration,
+    },
+  },
+  animate: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      duration,
+      delay: 0.1,
+      when: "beforeChildren",
+    },
+  },
+  exit: {
+    opacity: 0,
+    scale: 0.99,
+    transition: {
+      duration,
+      ease,
+    },
+  },
+};
+
+const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
     query BackgroundImageQuery {
       file(relativePath: { eq: "background.jpg" }) {
@@ -30,7 +61,17 @@ export default ({ children }) => {
 
       <Navbar />
 
-      <div className="mb-20">{children}</div>
+      <AnimatePresence>
+        <motion.main
+          variants={variants}
+          initial="initial"
+          animate="animate"
+          exit="exit"
+          className="mb-20"
+        >
+          {children}
+        </motion.main>
+      </AnimatePresence>
 
       <Search />
 
@@ -40,3 +81,5 @@ export default ({ children }) => {
     </div>
   );
 };
+
+export default Layout;
