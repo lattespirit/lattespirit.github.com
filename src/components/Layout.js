569bc2fde4e5c305e8cdd70b887837aeaf6af267
Jeffrey Yeung <4877706+lattespirit@users.noreply.github.com>
2025-04-23 23:40:49 +0800

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -11,11 +11,9 @@ import { useLocation } from "@reach/router";
 
 const variants = {
   initial: {
-    opacity: 0.8,
     y: -20,
   },
   animate: {
-    opacity: 1,
     y: 0,
     transition: {
       duration: 0.4,
