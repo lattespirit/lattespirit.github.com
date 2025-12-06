51bc2a3dfe732dbed9560d69189a353ac9e4b52f
Jeffrey Yeung <lattespirit@gmail.com>
2025-12-06 13:57:09 +0800

    chore: update dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ require("dotenv").config({
 module.exports = {
   siteMetadata: {
     url: process.env.SITE_URL,
-    title: "Lattespirit",
+    title: "Jeffrey Yeung",
     author: "Jeffrey Yeung",
     description: "Jeffrey Yeung's Blog",
   },
