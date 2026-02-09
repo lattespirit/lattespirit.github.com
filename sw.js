be85b34c332f13070d93dc5cbbaf1efcc9508ff7
lattespirit <lattespirit@users.noreply.github.com>
2026-02-09 17:09:57 +0000

    deploy: 9804c442057ad19b489ca41652c231d248a200c5

--- sw.js
+++ sw.js
@@ -27,20 +27,23 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-985e61aabe486f1def87.js"
+    "url": "webpack-runtime-f8e0a5a7625ae7827272.js"
+  },
+  {
+    "url": "styles.e433692444a7088a7513.css"
   },
   {
     "url": "framework-ed4e17d920f20e0049ea.js"
   },
   {
-    "url": "styles.cdad6da20b20f41f3cf3.css"
+    "url": "5c0b189e-59da9196baef69140768.js"
   },
   {
-    "url": "app-47439ab89131fa74973c.js"
+    "url": "app-c6e8f1476575c53fc825.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "11957e263d10e16a355c370808ba3704"
+    "revision": "08a54144fa91fa4c9015076ccc913e7b"
   },
   {
     "url": "manifest.webmanifest",
@@ -154,7 +157,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-47439ab89131fa74973c.js`))) {
+  if (!resources || !(await caches.match(`/app-c6e8f1476575c53fc825.js`))) {
     return await fetch(event.request)
   }
 
