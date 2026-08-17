942df73c73f7f2aa0202adf8941ec4b4948a802e
Jeffrey Yeung <lattespirit@gmail.com>
2026-08-17 13:00:22 +0800

    chore: fix iOS 26 text-wrap bug

--- src/components/Head.js
+++ src/components/Head.js
@@ -21,6 +21,10 @@ const Head = ({ title, description }) => {
   return (
     <>
       <title>{pageTitle}</title>
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, shrink-to-fit=no"
+      />
       <meta name="description" content={metaDescription} />
       <meta property="og:title" content={pageTitle} />
       <meta property="og:description" content={metaDescription} />
